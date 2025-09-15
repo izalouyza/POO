e1e782476d3d4b00802f6127e291cd9a8acbca43
liyuhi <whorshes@gmail.com>
2025-09-15 14:47:46 -0300

    Refactor: atualizando repositório

---
+++ codigos/aula5/SistemaRPGSala.java
@@ -0,0 +1,29 @@
+package aula5;
+
+import java.util.Scanner;
+
+public class SistemaRPGSala {
+    public static int menu(){
+        Scanner input = new Scanner(System.in);
+
+        int opcao;
+        System.out.println("""
+                Sistema RPG Sala:
+                
+                1 - Listar Personagens
+                2 - Atribuir XP
+                3 - Evoluir Personagens
+                4 - Adicionar Personagens            
+                5 - Remover Personagens
+                0 - Sair
+                
+                Opção:\t""");
+
+        opcao = input.nextInt();
+        return opcao;
+    }
+
+    public static void main(String[] args) {
+        int opcao = menu();
+    }
+}
